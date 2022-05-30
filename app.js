1271c95096b7551c2ede89ed71c0dba07e57e3c9
Vioque <jvioquea@hotmail.com>
2022-05-30 07:44:54 +0200

    Desarrollo de calculadora básica. Aún sin terminar. Interactuar con los valores de los inputs y botón suma.

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-var number1;
-var number2;
+// Primera parte. Comprobar las operaciones.
+// var number1 = 3;
+// var number2 = 2;
+//
+//
+// var sum = (number1, number2) => number1 + number2;
+// var rest = (number1, number2) => number1 - number2;
+// var mult = (number1, number2) => number1 * number2;
+// var div = (number1, number2) => number1 / number2;
+//
+//
+// console.log(sum(number1,number2));
+// console.log(rest(number1,number2));
+// console.log(mult(number1,number2));
+// console.log(div(number1,number2));
 
-document.getElementById("number1").addEventListener('change', number1 = document.getElementById("number1").value);
-document.getElementById("number2").addEventListener('change', number2 = document.getElementById("number2").value);
 
+var sum = () => {
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    result = number1 + number2
+}
+// var rest = (number1, number2) => number1 - number2;
+// var mult = (number1, number2) => number1 * number2;
+// var div = (number1, number2) => number1 / number2;
 
-console.log(number1());
-
-var sum = (number1, number2) => number1 + number2;
-var rest = (number1, number2) => number1 - number2;
-var mult = (number1, number2) => number1 * number2;
-var div = (number1, number2) => number1 / number2;
-
-
-console.log(sum(number1,number2));
-console.log(rest(number1,number2));
-console.log(mult(number1,number2));
-console.log(div(number1,number2));
+document.getElementById("result").innerHTML = result;
