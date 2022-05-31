45ca1ba396aff0cd41a2b474d4f1d370fadad68e
Vioque <jvioquea@hotmail.com>
2022-05-31 06:59:12 +0200

    Desarrollo de calculadora básica. Aún sin terminar. Interactuar con los valores de los inputs y botón suma. Práctica terminada.

--- app.js
+++ app.js
@@ -14,14 +14,35 @@
 // console.log(mult(number1,number2));
 // console.log(div(number1,number2));
 
+// Interactuar con los botones de operación
+var number1;
+var number2;
+var result;
+
 
 var sum = () => {
-    number1 = document.getElementById("number1").value;
-    number2 = document.getElementById("number2").value;
-    result = number1 + number2
+    number1 = document.getElementById('number1').valueAsNumber
+    number2 = document.getElementById('number2').valueAsNumber
+    result = isNaN(number1) || isNaN(number2) ? "Error" : number1 + number2
+    document.getElementById('result').innerHTML = result
 }
-// var rest = (number1, number2) => number1 - number2;
-// var mult = (number1, number2) => number1 * number2;
-// var div = (number1, number2) => number1 / number2;
+var rest = () => {
+    number1 = document.getElementById('number1').valueAsNumber
+    number2 = document.getElementById('number2').valueAsNumber
+    result = isNaN(number1) || isNaN(number2) ? "Error" : number1 - number2
+    document.getElementById('result').innerHTML = result
+}
+var mult = () => {
+    var number1 = document.getElementById('number1').valueAsNumber
+    var number2 = document.getElementById('number2').valueAsNumber
+    result = isNaN(number1) || isNaN(number2) ? "Error" : number1 * number2
+    document.getElementById('result').innerHTML = result
+}
+var division = () => {
+    var number1 = document.getElementById('number1').valueAsNumber
+    var number2 = document.getElementById('number2').valueAsNumber
+    result = isNaN(number1) || isNaN(number2) ? "Error" : number1 / number2
+    document.getElementById('result').innerHTML = result
+}
 
-document.getElementById("result").innerHTML = result;
+
